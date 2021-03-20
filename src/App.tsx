c31ede12586cbd0237adb7889efebfe0cc4c98f3
BayLadyCoder <rachada_32@hotmail.com>
2021-03-20 11:04:23 -0400

    Add Routers and Layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <header className="text-gray-600 body-font">
-    <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    {/* <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
           <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
@@ -25,7 +25,7 @@ function App() {
           <path d="M5 12h14M12 5l7 7-7 7"></path>
         </svg>
       </button>
-    </div>
+    </div> */}
 </header>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.scss';
 // import './index.css';
+import {BrowserRouter as Router} from 'react-router-dom'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Routes from './Routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
